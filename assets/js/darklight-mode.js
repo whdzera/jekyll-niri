d811fd0d524038cc7b65e1f86914dd0b539ef661
rokhimin <rokhim.whd@gmail.com>
2025-02-28 13:43:37 +0700

    changed structure css

--- assets/js/darklight-mode.js
+++ assets/js/darklight-mode.js
@@ -2,35 +2,36 @@ document.addEventListener("DOMContentLoaded", function () {
     const toggleButton = document.getElementById("toggle-button");
     const themeIcon = document.getElementById("theme-icon");
     const currentTheme = localStorage.getItem("theme");
-
-    // Light Mode sebagai default (Dark Mode hanya aktif jika sebelumnya dipilih)
-    if (currentTheme === "dark") {
-        document.documentElement.classList.add("is-dark");
+  
+    // Set default theme to Dark Mode
+    if (!currentTheme) {
+      document.documentElement.classList.add("is-dark");
+      localStorage.setItem("theme", "dark");
+    } else if (currentTheme === "dark") {
+      document.documentElement.classList.add("is-dark");
     } else {
-        document.documentElement.classList.remove("is-dark");
-        localStorage.setItem("theme", "light"); // Pastikan default tersimpan
+      document.documentElement.classList.remove("is-dark");
     }
-
+  
     function updateIcon() {
-        if (document.documentElement.classList.contains("is-dark")) {
-            themeIcon.classList.replace("fa-moon", "fa-sun");
-        } else {
-            themeIcon.classList.replace("fa-sun", "fa-moon");
-        }
+      if (document.documentElement.classList.contains("is-dark")) {
+        themeIcon.classList.replace("fa-moon", "fa-sun");
+      } else {
+        themeIcon.classList.replace("fa-sun", "fa-moon");
+      }
     }
-
+  
     // Perbarui ikon sesuai mode awal
     updateIcon();
-
+  
     toggleButton.addEventListener("click", () => {
-        document.documentElement.classList.toggle("is-dark");
-
-        if (document.documentElement.classList.contains("is-dark")) {
-            localStorage.setItem("theme", "dark");
-        } else {
-            localStorage.setItem("theme", "light");
-        }
-
-        updateIcon();
+      document.documentElement.classList.toggle("is-dark");
+      if (document.documentElement.classList.contains("is-dark")) {
+        localStorage.setItem("theme", "dark");
+      } else {
+        localStorage.setItem("theme", "light");
+      }
+      updateIcon();
     });
-});
+  });
+  
